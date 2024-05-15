464a5b58172e2f316cc67049cbac651689397871
jplaurent01 <66191438+jplaurent01@users.noreply.github.com>
2024-05-14 20:03:31 -0600

    Creacion MainFrame.hpp que utiliza wxwidget

---
+++ Previos/Previo10/Grabando_Clase/MainFrame.hpp
@@ -0,0 +1,25 @@
+#include <wx/wx.h>
+
+//Encabezado clase MainFrame
+class MainFrame : public wxFrame {
+	public:
+		//Constructor
+		MainFrame(const wxString& title);
+
+	private:
+		/*****MANEJO DINAMICO********/
+		//void OnButtonClicked(wxCommandEvent& evt);
+		//void OnSliderChanged(wxCommandEvent& evt);
+		//void OnTextChanged(wxCommandEvent& evt);
+		
+		/*****MANEJO ESTATICO********/
+		//Metodos privadosque involucran acciones
+		//Manejo estatico, acciones estaticas
+		//void OnButtonClicked(wxCommandEvent& evt);
+		//void OnSliderChanged(wxCommandEvent& evt);
+		//void OnTextChanged(wxCommandEvent& evt);
+		//wxDECLARE_EVENT_TABLE();
+		
+
+};
+
